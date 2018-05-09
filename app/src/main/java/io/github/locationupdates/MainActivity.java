fb0b661a06b61452023d181353d2ee56326879cc
xinyu1118 <yangxycl@163.com>
2018-05-09 13:16:58 -0400

    Add screenshots

--- app/src/main/java/io/github/locationupdates/MainActivity.java
+++ app/src/main/java/io/github/locationupdates/MainActivity.java
@@ -6,6 +6,9 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import io.github.privacysecurer.core.Event;
 import io.github.privacysecurer.core.GeolocationCallback;
@@ -18,6 +21,7 @@ import io.github.privacysecurer.utils.Globals;
 public class MainActivity extends AppCompatActivity {
     private Button mStartUpdatesButton;
     private Button mStopUpdatesButton;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
 
         mStartUpdatesButton = (Button) findViewById(R.id.start_updates_button);
         mStopUpdatesButton = (Button) findViewById(R.id.stop_updates_button);
+        textView = (TextView) findViewById(R.id.latlon);
+        textView.setPadding(2,400,2,2);
 
         mStartUpdatesButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
                         LatLon latLon = this.getLatLon();
                         Log.d("Log", "latitude: "+String.valueOf(latLon.getLatitude()));
                         Log.d("Log", "longitude: "+String.valueOf(latLon.getLongitude()));
+                        textView.setText("latitude: "+latLon.getLatitude()+" "+"longitude: "+latLon.getLongitude());
                     }
                 });
             }
